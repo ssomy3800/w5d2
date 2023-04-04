6b61762cbe46997c33468272ce49f5d53d38e405
joshua <joshk.5793@gmail.com>
2023-04-04 14:36:59 -0400

    finished up to 4

--- skeleton/lib/03a_nobel_bonus.rb
+++ skeleton/lib/03a_nobel_bonus.rb
@@ -11,5 +11,18 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+  SELECT
+    DISTINCT(yr)
+  FROM
+    nobels
+  WHERE
+    yr NOT IN (
+      SELECT
+        DISTINCT(yr)
+      FROM
+        nobels
+      WHERE 
+        subject = 'Chemistry'
+    ) AND subject = 'Physics';
   SQL
 end
